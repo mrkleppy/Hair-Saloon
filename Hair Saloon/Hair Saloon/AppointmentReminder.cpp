0c0192d0c05bca0111035e6b2585a1e2926aa4f6
TengLok0930 <tenglok0930liew@gmail.com>
2026-08-14 14:52:04 +0800

    addon for appt reminder
    ...

--- Hair Saloon/Hair Saloon/AppointmentReminder.cpp
+++ Hair Saloon/Hair Saloon/AppointmentReminder.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+<<<<<<< Updated upstream
+=======
+#include <ctime>
+>>>>>>> Stashed changes
 #include <format>
 #include "Main.h"
 #include "AppointmentModule.h"
 
 using namespace std;
 
+<<<<<<< Updated upstream
 Appointment findNearestAppointment(Customer customer, vector<Appointment>& customerAppointment) {
 	Appointment nearestAppointment;
 
@@ -31,18 +36,132 @@ Appointment findNearestAppointment(Customer customer, vector<Appointment>& custo
 		}
 	}
 
+=======
+void getCurrentDateTime(Date &currentDate, Time &currentTime) {
+	time_t now = time(nullptr);
+	tm local_tm{};
+
+	if (localtime_s(&local_tm, &now) != 0) {
+		return;
+	}
+
+	currentDate = { local_tm.tm_mday, local_tm.tm_mon + 1, local_tm.tm_year + 1900 }; // tm_mon start from (0 -11), tm_year start from year 1900
+	currentTime = { local_tm.tm_hour, local_tm.tm_min };
+}
+
+bool isEarlierDate(Date& a, Date& b) {
+	if (a.year != b.year) {
+		return a.year < b.year;
+	}
+
+	if (a.month != b.month) {
+		return a.month < b.month;
+	}
+
+	return a.day < b.day;
+}
+
+bool isEarlierTime(Time& a, Time& b) {
+	if (a.hour != b.hour) {
+		return a.hour < b.hour;
+	}
+
+	return a.minute < b.minute;
+}
+
+bool isEarlierAppointment(Appointment& a, Appointment& b) {
+	if (isEarlierDate(a.date, b.date)) {
+		return true;
+	}
+	if (isEarlierDate(b.date, a.date)) {
+		return false;
+	}
+
+	return isEarlierTime(a.time, b.time);
+}
+
+void compareDateTime(vector<Appointment>& customerAppointment, vector<Appointment>& upcomingAppointment) {
+	Date currentDate;
+	Time currentTime;
+	getCurrentDateTime(currentDate, currentTime);
+
+	//DEBUG USAGE//
+	/*
+	cout << "Current: "
+		<< currentDate.day << "/" << currentDate.month << "/" << currentDate.year << " "
+		<< currentTime.hour << ":" << currentTime.minute << "\n";
+	*/
+
+	Appointment nowAppt;
+	nowAppt.date = currentDate;
+	nowAppt.time = currentTime;
+
+	upcomingAppointment.clear();
+	for (Appointment appt : customerAppointment) {
+		if (!isEarlierAppointment(appt, nowAppt)) {
+			upcomingAppointment.push_back(appt);
+		}
+	}
+}
+
+Appointment findNearestAppointment(const vector<Appointment>& upcomingAppointment) {
+	if (upcomingAppointment.empty()) {
+		throw runtime_error("No upcoming appointments");
+	}
+
+	Appointment nearestAppointment = upcomingAppointment[0];
+
+	for (size_t i = 1; i < upcomingAppointment.size(); ++i) {
+		const Appointment& appt = upcomingAppointment[i];
+
+		if (appt.date.year < nearestAppointment.date.year) {
+			nearestAppointment = appt;
+		}
+		else if (appt.date.year == nearestAppointment.date.year) {
+			if (appt.date.month < nearestAppointment.date.month) {
+				nearestAppointment = appt;
+			}
+			else if (appt.date.month == nearestAppointment.date.month) {
+				if (appt.date.day < nearestAppointment.date.day) {
+					nearestAppointment = appt;
+				}
+				else if (appt.date.day == nearestAppointment.date.day) {
+					if (appt.time.hour < nearestAppointment.time.hour) {
+						nearestAppointment = appt;
+					}
+					else if (appt.time.hour == nearestAppointment.time.hour &&
+						appt.time.minute < nearestAppointment.time.minute) {
+						nearestAppointment = appt;
+					}
+				}
+			}
+		}
+	}
+
+>>>>>>> Stashed changes
 	return nearestAppointment;
 }
 
 void appointmentReminder(Customer customer, vector<Appointment> &appointments) {
+<<<<<<< Updated upstream
 	vector<Appointment> customerAppointment;
+=======
+	vector<Appointment> upcomingAppointment, customerAppointment;
+>>>>>>> Stashed changes
 	string appointmentDate;
 	string appointmentTime;
 	
 	loadCustomerPendingAppointments(customer, appointments, customerAppointment);
+<<<<<<< Updated upstream
 
 	if (customerAppointment.size() > 0) {
 		Appointment nearestAppointment = findNearestAppointment(customer, customerAppointment);
+=======
+	compareDateTime(customerAppointment, upcomingAppointment);
+
+	if (upcomingAppointment.size() > 0) {
+		Appointment nearestAppointment = findNearestAppointment(upcomingAppointment);
+>>>>>>> Stashed changes
 		appointmentDate = format("{:02d}/{:02d}/{:d}",nearestAppointment.date.day, nearestAppointment.date.month, nearestAppointment.date.year);
 		appointmentTime = format("{:02d}:{:02d}", nearestAppointment.time.hour, nearestAppointment.time.minute);
 
